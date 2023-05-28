3df62a0a28a092e28014a6c71492d743b60e5107
t233hao <youlongyang52@gmail.com>
2023-05-28 17:15:40 +0800

    Simplified Chinese

--- make.py
+++ make.py
@@ -12,7 +12,7 @@ from rich import print
 
 NO_LIMIT = False
 IS_TEST = False
-LANG = "Traditional Chinese"
+LANG = "Simplified Chinese"
 PROMPT = f"I want you to act as an {LANG} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in {LANG}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level {LANG} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations, the text is: "
 
 class Base:
